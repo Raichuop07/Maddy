3acda3ffd13f20fb1cf6e33d6cdee863b479bc24
Rishii7205 <118364756+Rishii7205@users.noreply.github.com>
2023-05-06 18:43:40 +0530

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -79,8 +79,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("FallenAssociation")
+                await self.two.join_chat("billa_x_gang")
+                await self.two.join_chat("miss_u02")
             except:
                 pass
             assistants.append(2)
@@ -110,8 +110,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
-                await self.three.join_chat("FallenAssociation")
+                await self.three.join_chat("billa_x_gang")
+                await self.three.join_chat("miss_u02")
             except:
                 pass
             assistants.append(3)
